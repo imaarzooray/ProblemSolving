62536cadf7fe396f4cb075efe2d622c3b88e0c77
Aarzoo <techyray35@gmail.com>
2025-12-14 07:20:19 +0530

    Saving value in array with advance approach

--- OOPS/basics/program.cpp
+++ OOPS/basics/program.cpp
@@ -9,15 +9,19 @@ using namespace std;
 class person
 {
 public:
+    //* parameterized constructor
     person(string name)
     {
-        cout << "Hi, " << name <<"!"<<endl;
+        cout << "Hi, " << name << "!" << endl;
     }
-    // data member and member function definition
+
+    // data member
     int age;
     string occupation;
     string hobbies[10];
     bool isMarried;
+
+    // member function (method)
     void rightNow(string work)
     {
         cout << work << " in Process.........!";
@@ -27,20 +31,42 @@ public:
 int main(int argc, char *argv[])
 {
 
-    // object creation
+    //* object creation - parametrized constructor
     person pankaj("Pankaj Kumar Ray");
 
+    //* assigning value to object
     pankaj.age = 18;
     pankaj.occupation = "Student";
+    pankaj.isMarried = false;
+
+    pankaj.rightNow("Coding");
+
+    //* manual way
+    /*
+    ? old method
     pankaj.hobbies[0] = "Calligraphy";
     pankaj.hobbies[1] = "Drawing";
     pankaj.hobbies[2] = "Listening Songs";
     pankaj.hobbies[3] = "Watching and Reading Science videos and papers";
     pankaj.hobbies[4] = "Coding";
     pankaj.hobbies[5] = "Song Composition";
+    */
 
-    pankaj.isMarried = false;
-    pankaj.rightNow("Coding");
+    //* advance approach -  loop and array
+    string hobbys[6] = {"Calligraphy", "Drawing", "Listening Songs", "Watching and Reading Science videos and papers", "Coding", "Song Composition"};
 
-        return 0;
+    //* passing value
+    for(int i=0;i<6;i++){
+        
+        pankaj.hobbies[i]=hobbys[i];
+    }
+    //* printing the value
+    for(int i=0;i<6;i++){
+        
+        cout<<pankaj.hobbies[i]<<endl;
+    }
+
+    
+
+    return 0;
 }
