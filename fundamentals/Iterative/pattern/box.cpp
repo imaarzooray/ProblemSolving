f71760418c7de43935dc5c85d7b5cc77289bd533
Aarzoo <techyray35@gmail.com>
2025-12-27 11:52:51 +0530

    Hollow Square Matrix

--- fundamentals/Iterative/pattern/box.cpp
+++ fundamentals/Iterative/pattern/box.cpp
@@ -2,10 +2,36 @@
 
 using namespace std;
 
+/*
+* * * * * *
+*
+*
+*
+*
+* * * * * *
+
+
+
+*/
+
 int main(void)
 {
 
-    
+    // outer loop
+    for(int i=0;i<6;i++)
+    {
+        for(int j=0;j<6;j++)
+        {
+            if((i==0 || i==5)||(j==0 || j==5))
+            {
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<"\n";
+    }
 
     return 0;
 }
