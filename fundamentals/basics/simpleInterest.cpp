9100cbf49a2eb9555f6c8ecf75f5bc360d44b522
Aarzoo <techyray35@gmail.com>
2025-12-28 10:49:51 +0530

    Simple Interest Program

---
+++ fundamentals/basics/simpleInterest.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(int argv, char *argc[])
+{
+    cout<<"Enter the following:\n";
+    float principal, time, rate;
+
+    cout<<"Principal: ";
+    cin>>principal;
+
+    cout<<"Rate: ";
+    cin>>rate;
+
+    cout<<"Time: ";
+    cin>>time;
+
+
+    float interest = (principal*rate*time)/100.0;
+
+
+
+    cout<<"Interest: "<<interest;
+    return 0;
+}
