c9a4a09b9fd6e8f2bd819f780fb337972ad3e059
Aarzoo <techyray35@gmail.com>
2025-12-28 15:47:10 +0530

    Max Element of the Row in Matrix

--- datastructures/array/matrices/maxRowElem.cpp
+++ datastructures/array/matrices/maxRowElem.cpp
@@ -1,12 +1,50 @@
 #include<iostream>
 using namespace std;
 
+
+int greatesOfThree(int num1, int num2, int num3)
+{
+     int greatest = 0;
+
+    if(num1>num2){
+        if(num2>num3){
+            greatest = num1;
+        }
+        else{
+            if(num3>num1)
+            {
+                greatest = num3;
+            }
+            else{
+                greatest =num1;
+            }
+        }
+    }
+    else{
+        if(num2>num3){
+            greatest = num2;
+
+        }
+        else{
+            if(num3>num1)
+            {
+                greatest = num3;
+            }
+            else{
+                greatest = num1;
+            }
+        }
+    }
+
+    return greatest;
+}
+
 int main(int argv, char *argc[])
 
 
 {
 
-    cout<<"Enter the Matrix: ";
+    cout<<"Enter the Matrix: \n";
     int matrix[3][3];
 
 
@@ -23,11 +61,15 @@ int main(int argv, char *argc[])
     
     for(int i=0;i<3;i++)
     {
+        int temp[3];
         for(int j=0;j<3;j++)
         {
-            cout<<"[ "<<i<<", "<<j<<"]: ";
-            cin>>matrix[i][j];
+           temp[j] = matrix[i][j];
         }
+
+       int greatest =  greatesOfThree(temp[0],temp[1],temp[2]);
+
+       cout<<"The greatest element is: "<<greatest<<endl;
     }
 
 
