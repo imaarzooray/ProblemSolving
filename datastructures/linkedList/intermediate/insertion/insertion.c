17d27692087bf44ec94632815cf57678c0aeb4b9
Aarzoo <techyray35@gmail.com>
2026-01-01 12:00:56 +0530

    Basic Structure of Insertion in Link List

--- datastructures/linkedList/intermediate/insertion/insertion.c
+++ datastructures/linkedList/intermediate/insertion/insertion.c
@@ -13,7 +13,32 @@ int main(int argv, char *argc[])
 
     struct node *newNode, *head, *temp;
     head = NULL;
-    newNode = (struct node *)malloc(sizeof(struct node));
+    
+    int choice;
+    while(choice)
+    {
+        printf("Choice - Continue(1) or Exit(0): ");
+        scanf("%d",&choice);
+        
+        newNode = (struct node *)malloc(sizeof(struct node));
+        printf("\nData:");
+        scanf("%d",newNode->data);
+
+        newNode->next = 0;
+
+        if(head==NULL)
+        {
+
+            head = newNode;
+            temp = newNode;
+        }
+        else{
+
+            temp->next = newNode;
+            temp = newNode;
+        }
+    
+    }
 
     
 
