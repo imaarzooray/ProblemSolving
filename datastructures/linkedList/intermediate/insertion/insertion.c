9c2fd02edd655c0ae417263066744f9d9015f47b
Aarzoo <techyray35@gmail.com>
2026-01-01 11:52:19 +0530

    Linked List: Insertion Program

---
+++ datastructures/linkedList/intermediate/insertion/insertion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+
+int main(int argv, char *argc[])
+{
+
+    struct node *newNode, *head, *temp;
+    head = NULL;
+    newNode = (struct node *)malloc(sizeof(struct node));
+
+    
+
+    return 0;
+}
