c10ab126f2b5d47e1358ba411d5e18853f97a784
Aarzoo <techyray35@gmail.com>
2026-01-07 22:37:21 +0530

    DP APLLIED BUT NOT SURE

--- algorithms/dynamicProgramming/fibbo.cpp
+++ algorithms/dynamicProgramming/fibbo.cpp
@@ -1,40 +1,77 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int sum, count;
+int sum, count, resp;
+
+int memo[5];
+
+int t = 0;
+
+
+int saveVal(int *arr,int val){
+
+    if(t!=5)
+    {
+
+        arr[t] = val;
+        t = t+1;
+    }
+    else{
+        return 1;
+    }
+
+}
+
+int linearSearch(int *arr, int track)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i] == track)
+        {
+            return 1;
+        }
+    }
+}
 
 int fibb(int n)
 {
+
+    resp  =linearSearch(memo,n);
+    if(resp==1)
+    {
+        return n;
+    }
     count++;
-    if(n<0)
+    if (n < 0)
     {
         printf("Invalid Input!");
         exit(0);
     }
-    if(n==0)
+    if (n == 0)
     {
         return 0;
     }
-    if(n==1)
+    if (n == 1)
     {
         return 1;
     }
-    sum = fibb(n-1) + fibb(n-2);
+    sum = fibb(n - 1) + fibb(n - 2);
+
+    saveVal(memo,sum);
     return sum;
 }
 
 int main(int argv, char *argc[])
 {
 
-    cout<<"Enter the value of the counter: ";
+    cout << "Enter the value of the counter: ";
     int counter;
-    cin>>counter;
+    cin >> counter;
 
-    int result = fibb(counter) + fibb(counter-1);
+    int result = fibb(counter) + fibb(counter - 1);
 
-    cout<<"The fibb functio is called "<<count<<" times.";
-    cout<<"\nThe sum of the "<<counter<<" is: "<<result;
-
+    cout << "The fibb functio is called " << count << " times.";
+    cout << "\nThe sum of the " << counter << " is: " << result;
 
     return 0;
 }
